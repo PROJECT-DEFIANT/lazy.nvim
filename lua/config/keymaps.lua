9fc7dd1e425915be07b2beca76e6b13d11c7551c
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-08-09 08:19:18 +0200

    personal changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<Leader>p", ":Telescope live_grep<CR>", { desc = "Fuzzy finder" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additio::q options here
+local options = {
+  -- backup = false, -- stop backing up the file
+  -- tabstop = 2, -- tab spans 2 spaces
+  -- relativenumber = true,
+  -- number = true,
+  -- shiftwidth = 2, -- clicking tab moves by 2 spaces
+  -- guifont = "monospace:h17",
+  -- fileencoding = "utf-8",
+  -- mouse = "a", -- enable mouse in the neovim
+  -- smartindent = true,
+  splitbelow = true, -- split horizontally will add tab on the bottom
+  splitright = true, -- split vertically will add tab on the right
+  -- wrap = false,
+  -- hidden = true,
+  -- completeopt = { "menuone", "noselect" },
+  -- updatetime = 300,
+}
+for opt_name, opt_value in pairs(options) do
+  vim.opt[opt_name] = opt_value
+end
